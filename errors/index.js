602ef0348f47a5ee89852f258ab07a939f755d2d
Alouche-Mustapha <mustapha.alouche.o@gmail.com>
2022-10-02 14:58:09 +0100

    adding custom error classes

---
+++ errors/index.js
@@ -0,0 +1,11 @@
+const CustomError = require("./custom-error");
+const NotFoundError = require("./not-found-error");
+const BadRequestError = require("./bad-request-error");
+const UnauthorizedError = require("./unauthorized-error");
+
+module.exports = {
+  CustomError,
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+};
