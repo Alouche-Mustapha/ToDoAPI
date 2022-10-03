01dda9397cbd28721b4be6f7233084c2c1d41e94
Alouche-Mustapha <mustapha.alouche.o@gmail.com>
2022-10-03 16:30:43 +0100

    adding task model and authentication middleware

---
+++ middlewares/authentication-middleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
+
+const authenticationMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthenticatedError(
+      "Authentication invalid , no token provided"
+    );
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const playload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: playload.userId, name: playload.name };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Not authorized to access this route");
+  }
+};
